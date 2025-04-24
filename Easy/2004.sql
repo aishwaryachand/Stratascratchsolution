f12f52bd13a8f7612a75f1e9ea45912c66684d45
Aishwarya Chand <51371189+aishwaryachand@users.noreply.github.com>
2025-04-24 02:27:50 -0400

    Update 2004.sql

--- Easy/2004.sql
+++ Easy/2004.sql
@@ -1,1 +1,9 @@
+--https://platform.stratascratch.com/coding/2004-number-of-comments-per-user-in-past-30-days?code_type=3
 
+select user_id , SUM(number_of_comments) 
+from fb_comments_count
+
+  WHERE  created_at BETWEEN DATE '2020-01-11' AND DATE '2020-02-09'
+  GROUP BY user_id
+  HAVING SUM(number_of_comments) > 0 ;
+  
